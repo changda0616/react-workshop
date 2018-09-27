98f6ff36c2ebf2541c03062675bbb04fba5e26a2
chadz_graphen <chadzhuang@graphen.ai>
2018-09-27 15:02:49 +0800

    Add handleCheckobxChange function in parent component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import logo from './logo.svg';
 import './App.scss';
 import Checkbox from './component/checkbox';
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+       isCheckboxChecked: false
+    }
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+  }
+  handleCheckboxChange(e){
+    this.setState({...this.state, isCheckboxChecked: e.target.checked});
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
-          <Checkbox handleChangeFromParent={(e)=>{console.log(e)}}/>
+          <Checkbox handleChangeFromParent={this.handleCheckboxChange}/>
         </header>
+        {`${this.state.isCheckboxChecked}`}
+
       </div>
     );
   }
